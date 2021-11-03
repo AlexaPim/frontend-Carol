4846b099c556728aa1afee1004e4c029cf73c10c
ivancarlos88 <ivancarlos.ms.21@gmail.com>
2021-11-03 15:03:00 -0300

    Adicao dos componentes Menu, Rodape, Entrar e Cadastrar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+
+const routes: Routes = [
+ {path:'',redirectTo:'entrar',pathMatch:'full'},
+ {path:'entrar', component: EntrarComponent},
+ {path:'cadastrar', component: CadastrarComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
